2fd170d0dde7e08fefb4168010b1db45d190f6d4
Jason Kirshner <jasonk@ruckusmarketing.com>
2022-12-15 00:45:14 -0500

    Preload hero and fonts
    Signed-off-by: Jason Kirshner <jasonk@ruckusmarketing.com>

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,7 +3,7 @@ import './styles.css'
 const Hero = () => {
   return (
     <div class="hero">
-      <img class="hero-image" src='/images/hero2.jpeg' alt="Spa room" />
+      <img class="hero-image" src='/images/hero.jpeg' alt="Spa room" />
       <h1 class="hero-title">Joanna Kay<br/>Aesthetics</h1>
     </div>
   )
