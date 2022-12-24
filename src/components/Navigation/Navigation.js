da4c9398217526b6c3d753f8175f4ffc2a57b3fa
Jason Kirshner <jasonk@ruckusmarketing.com>
2022-12-24 15:23:47 -0500

    Style mobile nav
    Signed-off-by: Jason Kirshner <jasonk@ruckusmarketing.com>

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,8 +1,10 @@
 import { useState } from 'react'
+import { Spin as Hamburger } from 'hamburger-react'
 import './styles.css'
 
 const Navigation = () => {
   const [isAnimClassAdded, setIsAnimClassAdded] = useState(false)
+  const [isMenuOpen, setMenuOpen] = useState(false)
 
   const navBar = document.querySelector('header')
 
@@ -45,10 +47,14 @@ const Navigation = () => {
     document.querySelector('.title-logo').classList.add('animated')
   }
 
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen)
+  }
+
   return (
     <header>
       <div class="header-container">
-        <button onClick={scrollToTop} class="title" onMouseOver={!isAnimClassAdded && addAnimClass}>
+        <button onClick={scrollToTop} className="title" onMouseOver={!isAnimClassAdded && addAnimClass}>
           <svg class="title-logo" preserveAspectRatio="none" viewBox="19.999 45.376 160.001 109.248" xmlns="http://www.w3.org/2000/svg">
             <path d="M120.23 154.624c-6.238 0-12.903-.92-19.976-2.761a2.286 2.286 0 0 1-1.709-2.033c-1.549-19.328 2.791-34.715 12.903-45.73 12.783-13.927 35.069-21.078 66.247-21.254.665.166 1.334.296 1.768.818.439.522.622 1.212.499 1.883-5.591 30.676-16.503 51.385-32.436 61.552-7.852 5.011-16.988 7.525-27.296 7.525zm-17.236-6.797c16.793 4.065 30.936 2.514 42.066-4.589 14.356-9.157 24.399-27.914 29.879-55.776-28.387.558-48.602 7.193-60.116 19.735-8.931 9.733-12.907 23.398-11.829 40.63z" data-color="1" class="icon1"/>
             <path d="M79.769 154.624c-10.31 0-19.443-2.51-27.297-7.525-15.934-10.167-26.847-30.876-32.436-61.552a2.29 2.29 0 0 1 2.255-2.701h.013c31.178.177 53.468 7.329 66.249 21.254 10.107 11.015 14.449 26.402 12.898 45.73a2.286 2.286 0 0 1-1.709 2.033c-7.071 1.84-13.737 2.761-19.973 2.761zM25.063 87.462c5.48 27.861 15.523 46.619 29.875 55.776 11.136 7.108 25.279 8.65 42.063 4.589 1.084-17.232-2.891-30.897-11.822-40.63C73.667 94.655 53.45 88.02 25.063 87.462zm74.105 62.185h.043-.043z" data-color="1" class="icon1"/>
@@ -56,7 +62,7 @@ const Navigation = () => {
           </svg>
           <p class="header-title-text p-cursive">Joanna Kay Aesthetics</p>
         </button>
-        <nav>
+        <nav className={'nav-menu ' + (isMenuOpen ? 'nav-menu-open' : '')}>
           <ul class="nav-items">
             <li class="nav-item">
               <button onClick={scrollToAboutMe} className='nav-item-link h3'>About Me</button>
@@ -69,6 +75,9 @@ const Navigation = () => {
             </li>
           </ul>
         </nav>
+        <div className="nav-hamburger">
+          <Hamburger toggle={toggleMenu} toggled={isMenuOpen} direction="right" color="#9c8c58" label="Show Menu" />
+        </div>
       </div>
     </header>
   )
