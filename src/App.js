5313f14240d8a49de03fee0b73e9d51575886d39
Jason Kirshner <jasonkirshner13@gmail.com>
2025-08-03 20:29:26 -0400

    feat: add AddOns component to main content in App
    Signed-off-by: Jason Kirshner <jasonkirshner13@gmail.com>

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import AboutMe from './components/AboutMe/AboutMe'
 import Environ from './components/Environ/Environ'
 import Hero from './components/Hero/Hero'
 import Services from './components/Services/Services'
+import AddOns from './components/AddOns/AddOns';
 import Instagram from './components/instagram/Instagram';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Hero />
       <div className="main-content">
         <Services />
+        <AddOns />
         <AboutMe />
         <Instagram />
         <Environ />
