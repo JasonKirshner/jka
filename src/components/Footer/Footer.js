2588574db62c0ca177da7367386ad36d7478681a
Jason Kirshner <jasonkirshner13@gmail.com>
2025-08-06 18:24:21 -0400

    feat: update footer to display store address instead of map link
    Signed-off-by: Jason Kirshner <jasonkirshner13@gmail.com>

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './styles.css'
 const Footer = () => {
   return (
     <footer>
-      <a href="https://www.google.com/maps/dir/?api=1&destination=85+N.+3rd+Street+Suite+#3+Brooklyn+NY" target="_blank" rel="noreferrer" className="store-info">85 N. 3rd Street Suite # 3<br/>Brooklyn NY 11249</a>
+      <p className="store-info">Brooklyn, NY 11222</p>
       <a href="tel:8589995092" className="store-info">858-999-5092</a>
       <a href="mailto:joannakayaesthetics@gmail.com" className="store-info">joannakayaesthetics@gmail.com</a>
     </footer>
