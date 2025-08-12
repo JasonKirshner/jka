006ad826779fb811c4649c7bfffebff1880bfd99
Jason Kirshner <jasonkirshner13@gmail.com>
2025-08-11 21:19:20 -0400

    feat: update footer to include Rockland, NY in store information
    Signed-off-by: Jason Kirshner <jasonkirshner13@gmail.com>

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './styles.css'
 const Footer = () => {
   return (
     <footer>
-      <p className="store-info">Brooklyn, NY 11222</p>
+      <p className="store-info">Brooklyn, NY & Rockland, NY</p>
       <a href="tel:8589995092" className="store-info">858-999-5092</a>
       <a href="mailto:joannakayaesthetics@gmail.com" className="store-info">joannakayaesthetics@gmail.com</a>
     </footer>
